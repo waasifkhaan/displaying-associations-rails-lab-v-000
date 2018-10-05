88fb06831ced9a3d3efb46219e3d15f8a4975549
Wasif Khan <waasif.khaan@gmail.com>
2018-10-05 15:49:42 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,10 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
@@ -41,6 +43,8 @@ class ArtistsController < ApplicationController
     flash[:notice] = "Artist deleted."
     redirect_to artists_path
   end
+ 
+  
 
   private
 
